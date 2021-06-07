6cf5e6681dcad209b47ba25396728c9d52b007c4
LF Bittencourt <github@lfbittencourt.com>
2021-06-06 22:06:33 -0300

    Add POC

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input
+      type="number"
+      :min="minYear"
+      :max="relativeMaxYear"
+      v-model.number="startYear"
+    >
+    -
+    <input
+      type="number"
+      :min="relativeMinYear"
+      :max="maxYear"
+      v-model.number="endYear"
+    >
+    <Chart
+      :xLabels="currentYears"
+      :datasets="datasets"
+      :datasetLabels="datasetLabels"
+      :datasetColors="datasetColors"
+    />
+    <label v-for="feature in availableFeatures" :key="feature">
+      <input type="checkbox" :value="feature" v-model="selectedFeatures">
+      {{ uppercase(feature) }}
+    </label>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import Chart from '@/components/Chart.vue';
+import data from '@/data/database.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Chart,
+  },
+  data() {
+    return {
+      startYear: null,
+      endYear: null,
+      selectedFeatures: [],
+      availableDatasetColors: [ // Special thanks to https://learnui.design/tools/data-color-picker.html
+        '#003f5c',
+        '#2f4b7c',
+        '#665191',
+        '#a05195',
+        '#d45087',
+        '#f95d6a',
+        '#ff7c43',
+        '#ffa600',
+        '#009e49',
+        '#00bcf2',
+      ],
+    };
+  },
+  computed: {
+    availableFeatures() {
+      return data.columns;
+    },
+    minYear() {
+      return Math.min(...data.index);
+    },
+    maxYear() {
+      return Math.max(...data.index);
+    },
+    relativeMinYear() {
+      return Math.max(this.minYear, this.startYear);
+    },
+    relativeMaxYear() {
+      return Math.min(this.maxYear, this.endYear);
+    },
+    startYearIndex() {
+      return data.index.indexOf(this.startYear);
+    },
+    endYearIndex() {
+      return data.index.indexOf(this.endYear);
+    },
+    currentYears() {
+      return data.index.slice(this.startYearIndex, this.endYearIndex + 1);
+    },
+    currentData() {
+      return data.data.slice(this.startYearIndex, this.endYearIndex + 1);
+    },
+    selectedFeaturesIndexes() {
+      return this.selectedFeatures.map((feature) => this.availableFeatures.indexOf(feature));
+    },
+    datasets() {
+      return this.selectedFeaturesIndexes.map(
+        (index) => this.currentData.map((yearData) => yearData[index]),
+      );
+    },
+    datasetLabels() {
+      return this.selectedFeatures.map((column) => this.uppercase(column));
+    },
+    datasetColors() {
+      return this.selectedFeaturesIndexes.map((index) => this.availableDatasetColors[index]);
+    },
+  },
+  methods: {
+    uppercase(featureName) {
+      return featureName.charAt(0).toUpperCase() + featureName.slice(1);
+    },
+  },
+  mounted() {
+    this.startYear = this.minYear;
+    this.endYear = this.maxYear;
+    this.selectedFeatures = this.availableFeatures;
   },
 };
+
 </script>
 
 <style lang="scss">
---
+++ src/components/Chart.vue
@@ -0,0 +1,54 @@
+<template>
+  <canvas width="400" height="100"></canvas>
+</template>
+
+<script>
+
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+export default {
+  name: 'Chart',
+  props: {
+    xLabels: Array,
+    datasets: Array,
+    datasetLabels: Array,
+    datasetColors: Array,
+  },
+  data() {
+    return {
+      chart: null,
+    };
+  },
+  computed: {
+    chartDatasets() {
+      return this.datasets.map((data, index) => ({
+        label: this.datasetLabels[index],
+        borderColor: this.datasetColors[index],
+        data,
+      }));
+    },
+  },
+  mounted() {
+    const ctx = this.$el.getContext('2d');
+
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: this.xLabels,
+        datasets: this.chartDatasets,
+      },
+    });
+  },
+  watch: {
+    datasets() {
+      this.chart.data.labels = this.xLabels;
+      this.chart.data.datasets = this.chartDatasets;
+
+      this.chart.update();
+    },
+  },
+};
+
+</script>
